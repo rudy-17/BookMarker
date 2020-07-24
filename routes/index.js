c113deb3bec2aa00e85616264ae6b45b7f87f0fa
Rudresh17 <rudreshlegend@gmail.com>
2020-07-24 10:24:55 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const {ensureAuth,ensureGuest}= require('../middleware/auth')
 const Bookmark=require('../models/bookmark')
+const https = require('https')
 
 
 
@@ -19,8 +20,15 @@ router.get('/dashboard',ensureAuth, async(req,res)=>
     try
     {
         const bookmarks= await Bookmark.find({user: req.user.id}).lean()
+        https.get(bookmarks[1].link,(res)=>
+        {
+            
+        })
+        console.log(bookmarks)
+        //console.log(req.user)
         res.render('dashboard',
         {
+        image:req.user.image,
         name: req.user.firstName,
         bookmarks 
         })
